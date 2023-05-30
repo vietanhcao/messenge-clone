b9a41c5d43d2f5bfdf22757083b904ac9448504c
vietanhcao <vietanhcao1994@gmail.com>
2023-05-30 16:47:22 +0700

    feat(init): init next and create site page

---
+++ app/(site)/page.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+export default function Home() {
+	return (
+		<div
+			className="
+        flex
+        min-h-full
+        justify-center
+        items-center
+        py-12
+        sm:px-6
+        lg:px-8
+        bg-gray-100
+        "
+		>
+			<div className="sm:mx-auto sm:w-full sm:max-w-md">
+				<Image
+					className="mx-auto w-auto"
+					src="/images/logo.png"
+					width={48}
+					height={48}
+					alt="logo"
+				/>
+				<h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900 ">
+					Sign in to your account
+				</h2>
+			</div>
+			{/* AuthForm */}
+		</div>
+	);
+}
