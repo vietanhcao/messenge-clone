a742bb008542a2def8623643dc9514d196c38e94
vietanhcao <vietanhcao1994@gmail.com>
2023-05-31 17:23:46 +0700

    feat(init): add auth form and added some dependencies

---
+++ app/(site)/components/AuthSocialButton.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { IconType } from "react-icons";
+
+interface AuthSocialButtonProps {
+	icon: IconType;
+	onClick: () => void;
+}
+
+const AuthSocialButton: React.FC<AuthSocialButtonProps> = ({
+	icon: Icon,
+	onClick,
+}) => {
+	return (
+		<button
+			onClick={onClick}
+			type="button"
+			className="
+      inline-flex
+      w-full
+      justify-center
+      roaunded-md
+      bg-white
+      px-4
+      py-2
+      text-gray-500
+      showdow-sm
+      ring-1
+      ring-inset
+      ring-gray-300
+      hover:bg-gray-50
+      focus:outline-offset-0
+    "
+		>
+			<Icon />
+		</button>
+	);
+};
+
+export default AuthSocialButton;
