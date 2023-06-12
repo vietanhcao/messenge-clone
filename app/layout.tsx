3730530f6f5308b9fd6eaffd414a50f784210a29
vietanhcao <vietanhcao1994@gmail.com>
2023-06-13 00:23:46 +0700

    feat(chore): fix bug activeStatus

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,12 +17,10 @@ export default async function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
-	const currentUser = await getCurrentUser();
-
 	return (
 		<html lang="en">
 			<body className={inter.className}>
-				{currentUser && <ActiveStatus />}
+				<ActiveStatus />
 				<AuthContext>
 					<ToasterContext />
 					{children}
